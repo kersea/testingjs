69ea5902e0879b446bc1177d194e6dcef049ecbc
kersea <kerseaw@gmail.com]>
2023-05-13 22:03:56 -0700

    changes made to the name and last alert

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let firstName = prompt("What's your first name?");
+var firstName = prompt("What's your first name?");
 
-let lastName = prompt("What's your last name?");
+var lastName = prompt("What's your last name?");
 
-let fullName = (firstName + " " + lastName).toUpperCase;
+var fullName = (firstName + " " + lastName).toUpperCase;
 
-let fullNameLength = upperCaseName.length;
+var characterCount = fullName.length;
 
 alert("The string "+"fullName" +" is " + fullNameLength + " number of characters long.");
-
+alert("The string \"" + fullName + "\" is " + characterCount + " number of characters long.");
 //this is how you get the string to count all the characters in the string to get a total number of characters.
